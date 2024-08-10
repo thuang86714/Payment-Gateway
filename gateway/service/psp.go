050b452ecc81e926b24c23694eb5c006ca876c83
Hsien Heng Tommy Huang <thuang0714@gmail.com>
2024-08-10 23:57:54 +0800

    feat: implement payment service provider(PSP) in gateway/service 	Add PSPFactory for creating transactions with different Payment Service Providers 	Implement NewTransasactionWithPSP functions to select a PSP and 	create a transaction 	Add placeholder functions for PSP and fee percentage selection 	Include unit test for NewTransactionWithPSP function

---
+++ gateway/service/psp.go
@@ -0,0 +1,58 @@
+package service
+
+import (
+	"math/rand"
+
+	"github.com/processout-hiring/payment-gateway-thuang86714/shared/models"
+)
+
+// PSPFactory creates a new PSP transaction
+type PSPFactory struct {
+	ProviderName string
+}
+
+func (p PSPFactory) newTransaction(invoice models.Invoice, invoiceID string, feePercentage float64) models.TransactionWithPSP {
+	return models.TransactionWithPSP{
+		InvoiceID:       invoiceID,
+		AmountPayable:   float64(invoice.Total),
+		ServiceFee:      float64(invoice.Total) * feePercentage,
+		AmountReceived:  float64(invoice.Total) - float64(invoice.Total)*feePercentage,
+		CardNumber:      invoice.CardNumber,
+		CVV:             invoice.CVV,
+		ExpirationDate:  invoice.ExpirationDate,
+		ServiceProvider: p.ProviderName,
+	}
+}
+
+var funcMap = map[int]PSPFactory{
+	0: {ProviderName: "Checkout.com"},
+	1: {ProviderName: "Stripe"},
+	2: {ProviderName: "Klarna"},
+	3: {ProviderName: "Square"},
+	4: {ProviderName: "WorldPay"},
+	5: {ProviderName: "Paypal"},
+}
+
+func NewTransactionWithPSP(curInvoice models.Invoice, invoiceID string) models.TransactionWithPSP {
+	//decided which PSP benefit the merchant the most
+	curPSP, feePercentage := decidePSP()
+	var createTransactionWithPSP = funcMap[curPSP]
+
+	return createTransactionWithPSP.newTransaction(curInvoice, invoiceID, feePercentage)
+}
+
+// TODO: decidePSP is the core value of PSP platform, requiring complicated processes
+// Here we use rand to make a simple representation
+// decidePSP() decideds best PSP for this transaction, return int
+func decidePSP() (int, float64) {
+	percentage := decideServiceFeePercentage()
+	return rand.Intn(6), percentage
+}
+
+// TODO: decideServiceFee is the core value of PSP platform, requiring complicated processes
+// Here we use rand to make a simple representation
+// decidePSP() decideds best PSP for this transaction, return float64
+func decideServiceFeePercentage() float64 {
+	feePercentage := float64(rand.Intn(100)+1) / float64(10000)
+	return feePercentage
+}
