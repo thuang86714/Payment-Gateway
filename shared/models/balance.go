e0a620398a60d9c926ad7e460c8cd937b1074d5d
Hsien Heng Tommy Huang <44539518+thuang86714@users.noreply.github.com>
2024-08-13 22:13:32 +0800

    doc: add comments for struct purpose

--- shared/models/balance.go
+++ shared/models/balance.go
@@ -1,5 +1,6 @@
 package models
 
+//type balance works as a schema for db Auto-migration
 type Balance struct {
     Currency string  `gorm:"primaryKey" json:"currency"`
     Amount   float64 `json:"amount"`
